65b3bf47125ca8300505879348d118a3a2054e9f
Angel Toxqui <angeltoxquimunoz96@gmail.com>
2026-01-06 15:06:40 -0500

    MENU

--- frontend/finance-app-front/src/components/layout/MainLayout.tsx
+++ frontend/finance-app-front/src/components/layout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
-import { Menu } from 'lucide-react';
+import { Menu, Wallet } from 'lucide-react';
 
 const MainLayout = () => {
   return (
